2dbf1c036a01eec9ad723b066f89f1dd2addde1b
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-09-12 20:13:13 +0300

    Fix tests

--- builder/builder_test.go
+++ builder/builder_test.go
@@ -311,16 +311,17 @@ func TestBuildRollupTxs(t *testing.T) {
 	require.Len(t, ethTxs, 2, "Expected two Ethereum transactions: L1 attribute and deposit tx, got %d", len(ethTxs))
 
 	depositTxETH := ethTxs[1]
-	require.NotNil(t, depositTxETH.Value())
+	require.NotNil(t, depositTxETH.Mint())
 	require.NotNil(t, depositTxETH.To(), "Deposit transaction must have a 'to' address")
 
-	cosmAddr := utils.EvmToCosmosAddress(*depositTxETH.To())
+	to, err := gethtypes.NewLondonSigner(depositTxETH.ChainId()).Sender(depositTxETH)
+	require.NoError(t, err)
+	cosmAddr := utils.EvmToCosmosAddress(to)
 	withdrawalTx := testapp.ToWithdrawalTx(
 		t,
 		cosmAddr.String(),
 		common.HexToAddress("0x12345abcde").String(),
-		math.NewIntFromBigInt(depositTxETH.Value()),
-		big.NewInt(100_000),
+		math.NewIntFromBigInt(depositTxETH.Mint()),
 	)
 
 	b := builder.New(
@@ -346,7 +347,7 @@ func TestBuildRollupTxs(t *testing.T) {
 	builtBlock, preBuildInfo, postBuildInfo := buildBlock(t, b, env.app, payload)
 
 	// Test deposit was received
-	checkDepositTxResult(t, env.txStore, depositTxs, fmt.Sprintf("%sETH", depositTxETH.Value().String()), cosmAddr.String())
+	checkDepositTxResult(t, env.txStore, depositTxs, fmt.Sprintf("%sETH", depositTxETH.Mint().String()), cosmAddr.String())
 
 	withdrawalTxResult, err := env.txStore.Get(bfttypes.Tx(withdrawalTx).Hash())
 	require.NoError(t, err)
